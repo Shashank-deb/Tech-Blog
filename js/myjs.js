057d1a73d4978b4fb925fa5cb27cad468b908fd3
Shashank Sharma <sharmashashank446@gmail.com>
2022-04-07 08:34:54 +0530

    adding the js folder

---
+++ js/myjs.js
@@ -0,0 +1,20 @@
+  function doLike(pid,uid){
+    console.log(pid+","+uid);
+    const d={
+        uid:uid,
+        pid:pid,
+        operation:'like'
+        }
+        $.ajax({
+            url:"LikeServlet",
+            data:d,
+           success:function(data,textStatus,jqXHR){
+           console.log(data);
+        },
+        error:function (jqXHR,textStatus,errorThrown) {
+            console.log(data);
+        }    
+        })
+        
+    
+}
